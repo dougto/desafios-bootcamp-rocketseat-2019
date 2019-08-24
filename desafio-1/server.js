99b75a3dbaa43de5f79aa391186931c2d03f1e7e
Douglas Toledo <douglas22toledo@gmail.com>
2019-08-24 09:56:54 -0300

    Challenge completed

--- desafio-1/server.js
+++ desafio-1/server.js
@@ -2,25 +2,50 @@ const express = require('express');
 
 const server = new express();
 
-const projects = [
-  { id: '1', title: 'projeto 1', tasks: [] },
-  { id: '2', title: 'projeto 2', tasks: [] },
-  { id: '3', title: 'projeto 3', tasks: [] }
-];
+const projects = [];
 
 const state = {
-  projectNotFound: false,
-  currentIndex: 0
+  projectFound: false,
+  currentIndex: 0,
+  totalRequests: 0
 };
 
 server.use(express.json());
+
+// Middlewares:
+
+// Confere se o projeto existe
+function validateId(req, res, next) {
+  const { id } = req.params;
+
+  projects.map(el => {
+    if (el.id == id) {
+      state.projectFound = true;
+    }
+  });
+
+  if (!state.projectFound) {
+    return res.status(400).json({ error: 'Project Not Found' });
+  }
+
+  state.projectFound = false;
+
+  return next();
+}
+
+// Incrementa a contagem de requisições feitas
+function countRequests(req, res, next) {
+  state.totalRequests++;
+  console.log(state.totalRequests);
+  return next();
+}
 
 /* 
 A rota deve receber id e title dentro corpo de cadastrar um novo projeto dentro de um 
 array no seguinte formato: { id: "1", title: 'Novo projeto', tasks: [] }; Certifique-se 
 de enviar tanto o ID quanto o título do projeto no formato string com àspas duplas.
 */
-server.post('/projects', (req, res) => {
+server.post('/projects', countRequests, (req, res) => {
   const { id, title, tasks } = req.body;
 
   const newProject = { id: '' + id, title: '' + title, tasks: tasks };
@@ -30,12 +55,12 @@ server.post('/projects', (req, res) => {
 });
 
 // Rota que lista todos projetos e suas tarefas;
-server.get('/projects', (req, res) => {
+server.get('/projects', countRequests, (req, res) => {
   return res.json(projects);
 });
 
 // A rota deve alterar apenas o título do projeto com o id presente nos parâmetros da rota;
-server.put('/projects/:id', (req, res) => {
+server.put('/projects/:id', countRequests, validateId, (req, res) => {
   const { title } = req.body;
 
   projects.map(el => {
@@ -49,21 +74,15 @@ server.put('/projects/:id', (req, res) => {
 });
 
 // A rota deve deletar o projeto com o id presente nos parâmetros da rota;
-server.delete('/projects/:id', (req, res) => {
+server.delete('/projects/:id', countRequests, validateId, (req, res) => {
   const { id } = req.params;
 
   projects.map(el => {
     if (el.id == id) {
       projects.splice(projects.indexOf(el), 1);
       console.log('Project Deleted Successfully');
-      state.projectNotFound = true;
     }
   });
-
-  if (!state.projectNotFound) {
-    console.log('Project Not Found.');
-  }
-  state.projectNotFound = false;
 
   return res.json(projects);
 });
@@ -77,7 +96,7 @@ nota pessoal: Acho que faz muito mais sentido enviar os tasks pelo corpo
 da requisição, e não pela query string. Por esse motivo, esse post apenas
 receberá a id do projeto em questão na rota.
 */
-server.post('/projects/:id', (req, res) => {
+server.post('/projects/:id', countRequests, validateId, (req, res) => {
   const { tasks } = req.body;
   const { id } = req.params;
 
